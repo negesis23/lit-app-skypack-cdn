042b69670596366583f0c4494d6e97b0c4f9fcd5
Negesis23 <rangganopian50@gmail.com>
2023-08-16 15:09:18 +0700

    .

--- index.js
+++
@@ -1,9 +0,0 @@
-import './App.js';
-import './routes.js';
-
-
-if ('serviceWorker' in navigator) {
- navigator.serviceWorker.register('sw.js')
- .then(() => console.log('SW registered'))
- .catch(err => console.error('SW Error:', err));
-}
